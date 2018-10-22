6517d50dfcca660a4d588edcec083169751bea86
Jack Danielski <jackdski555@gmail.com>
2018-10-22 00:41:25 -0600

    switched to shift registers

--- tubes.h
+++ tubes.h
@@ -1,97 +1,38 @@
 /*
- *
  * Jack Danielski
  * VFD Tube Clock
  *  tubes.h
- *
  */
 
-#ifndef TUBES_H_
-#define TUBES_H_
-
 #include "msp.h"
-#include "rtc.h"
+#include <stdint.h>
 
-#define ONEA    11
-#define ONEB    12
-#define ONEC    13
-#define ONED    14
-#define ONEE    15
-#define ONEF    16
-#define ONEG    17
-#define ONEDP   18
-#define TWOA    20
-#define TWOB    21
-#define TWOC    22
-#define TWOD    23
-#define TWOE    24
-#define TWOF    25
-#define TWOG    26
-#define TWODP   27
-#define THREEA  30
-#define THREEB  31
-#define THREEC  32
-#define THREED  33
-#define THREEE  34
-#define THREEF  35
-#define THREEG  36
-#define THREEDP 37
-#define FOURA   40
-#define FOURB   41
-#define FOURC   42
-#define FOURD   43
-#define FOURE   44
-#define FOURF   45
-#define FOURG   46
-#define FOURDP  47
-#define FIVEA   50
-#define FIVEB   51
-#define FIVEC   52
-#define FIVED   53
-#define FIVEE   54
-#define FIVEF   55
-#define FIVEG   56
-#define FIVEDP  57
-#define SIXA    60
-#define SIXB    61
-#define SIXC    62
-#define SIXD    63
-#define SIXE    64
-#define SIXF    65
-#define SIXG    66
-#define SIXDP   67
+/* used for shift register delays */
+void enableSystick(uint16_t microseconds);
 
+void disableSystick();
 
-typedef struct {
-    uint8_t port;       // use '1' to signify port 1
-    uint8_t bitnum;     // bit 0-9 of Port X
-} vfdSeg_t;
+void SysTick_Handler();
 
-typedef struct {
-    vfdSeg_t a;
-    vfdSeg_t b;
-    vfdSeg_t c;
-    vfdSeg_t d;
-    vfdSeg_t e;
-    vfdSeg_t f;
-    vfdSeg_t g;
-    vfdSeg_t dp;
-} vfdTube_t;
+// 0-6 for select tubes, 7 for all
+void disableOutput(uint8_t target);
 
-vfdSeg_t * create_vfd_seg( uint8_t pb );//uint8_t port, uint8_t bitnumber);
+void enableOutput(uint8_t target);
 
-void makeVFDsegs();
+void pulseClock();
 
-void update_seg_on(vfdSeg_t * seg);
+void configureShiftPins();
 
-void update_seg_off(vfdSeg_t * seg);
+uint8_t decToSevSeg(uint8_t value);
 
-vfdTube_t * create_vfd_tube();
+void assignPin(uint8_t tube, uint8_t val);
 
-void sevensegConversion(vfdTube_t * current, uint8_t value);
+void updateSeconds(uint8_t value);
 
-void dec_to_seven_seg(vfdTube_t * firstVFD, vfdTube_t * secondVFD, uint8_t hms);
+void updateMinutes(uint8_t value);
 
-void update_tubes(uint8_t hours, uint8_t minutes, uint8_t seconds);
+void updateHours(uint8_t value);
 
-#endif
+void updateTime(uint8_t decHrs, uint8_t decMins, uint8_t decSecs);
+
+void shiftOut(uint8_t tubeNumber, uint8_t val);
