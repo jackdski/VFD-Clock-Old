bd55d6e8b9eb3d4c5fd7f528e7308cd15a20cb8c
Jack Danielski <jada0775@colorado.edu>
2019-02-05 23:46:55 -0800

    added I2C driver and temp sensor sampling, does not receive messages back

--- main.c
+++ main.c
@@ -29,6 +29,8 @@ volatile uint8_t seconds = 0;//0b1111111;
 
 CircBuf_t * TXBuf;
 CircBuf_t * RXBuf;
+CircBuf_t * RXBuf_i2c;
+CircBuf_t * TXBuf_i2c;
 
 //volatile uint8_t onOff = 1;         // 0 = blanks, 1 = current time
 //volatile uint8_t setupChanged = 0;  // for setup mode
@@ -46,105 +48,27 @@ void main(void) {
 	// create circular buffer for Bluetooth comm
     RXBuf = createCircBuf(8);
     TXBuf = createCircBuf(16);
+    RXBuf_i2c = createCircBuf(8);
+    TXBuf_i2c = createCircBuf(8);
 
     // configuration function calls
 	configure_all_pins();
 	configure_SystemClock();
-	configure_rtc();
+//	configure_rtc();
 	configure_uart();
+	configure_i2c();
 	configure_buttons();
 	configure_leds();
-	configure_shift_pins();
+//	configure_shift_pins();
 
 	__enable_irq();
-
-
-    updateTime(hours, minutes, seconds);
-    configure_rtc();    // now config RTC
-
+    P1->OUT &= ~BIT0;
+    uint8_t temp;
 	/* MAIN LOOP */
 	while(1) {
-	    // BLUETOOTH SETUP mode
-	    if( P1->IN & BIT4 ) {
-	        if( isFullCircBuf(RXBuf) ) {
-                uint8_t str[8];
-                int i = 0;
-                for(i=0; i < 8; i++) {
-                    str[i] = removeItem(RXBuf) - 48;
-                }
-
-                if(str[0] == '<' && str[7] == '>' ){
-                    // take RXBuf items and convert from ASCII to decimal
-                    hours = str[1] + str[2];
-                    minutes = str[3] + str[4];
-                    seconds = str[5] + str[6];
-                    updateTime(hours, minutes, seconds);
-                }
-                else {  // print error message to try again
-                    uint8_t * string = "ERROR...\n";
-                    loadToBuf(TXBuf, string, 9);
-                    if(!(isEmpty((CircBuf_t *)TXBuf))) {
-                        EUSCI_A0->IFG |= BIT1;
-                    }
-
-                    string = "Try again...\n";
-                    loadToBuf(TXBuf, string, 13);
-                    if(!(isEmpty((CircBuf_t *)TXBuf))) {
-                        EUSCI_A0->IFG |= BIT1;
-                    }
-                }
-                if( (hours == 1) && (minutes == 2) && (seconds == 3) ) {
-                    // light up green light
-                    P2->OUT &= ~(BIT0 | BIT2);
-                    P2->OUT |= BIT1;
-                }
-                else {
-                    // light up red light
-                    P2->OUT &= ~(BIT1 | BIT2);
-                    P2->OUT |= BIT0;
-                }
-                resetCircBuf((CircBuf_t *)RXBuf);
-	        }
-	    }
-
-	    // MANUAL SETUP position - switch is HIGH so run time-picking mode
-	    if( P5->IN & BIT0 ) {
-	        // turn on setup indication LED
-	        P1->OUT |= BIT0;
-
-	        // if not disabled already, disable RTC interrupts
-            if(NVIC->ISER[(((uint32_t)(int32_t)RTC_C_IRQn) >> 5UL)] !=
-                    (uint32_t)(1UL << (((uint32_t)(int32_t)RTC_C_IRQn) & 0x1FUL))) {
-                NVIC_DisableIRQ(RTC_C_IRQn);
-            }
-
-            // if + button is pressed
-            if(doButtons &= 0b1) {
-                if(buttonCount >= 10)
-                    doButtons = 0b1001;
-            }
-            // if - button is pressed
-            if(doButtons &= 0x10) {
-                if(buttonCount >= 10)
-                    doButtons = 0b10010000;
-            }
-
-            // update tubes if safe for reading (necessary to check?)
-            if(RTCCTL13 & RTC_C_CTL13_RDY) { // if safe for reading
-                updateTime(RTCHOUR, RTCMIN, RTCSEC);
-            }
-	        P1->OUT |= BIT0; // indicate SETUP
-	    }
-
-	    // NORMAL position - waits for interrupts
-	    else { // !(P5->IN & BIT0)
-	        // if not enabled already, enable RTC interrupts
-	        if(NVIC->ISER[(((uint32_t)(int32_t)RTC_C_IRQn) >> 5UL)] ==
-	                (uint32_t)(1UL << (((uint32_t)(int32_t)RTC_C_IRQn) & 0x1FUL))) {
-	            NVIC_EnableIRQ(RTC_C_IRQn);
-	            buttonCount = 0; // reset button counter too
-	        }
-	        P1->OUT &= ~BIT0; // indicate NORMAL
-	    }
+	    temp = read_temp_f();
+	    send_byte(temp); // not sent in ASCII
+	    int i;
+	    for(i=0;i < 100000; i++);
 	}
 }
--- peripherals.c
+++ peripherals.c
@@ -12,6 +12,19 @@
 #include "timer.h"
 #include "circbuf.h"
 
+/*
+ * MPL3115A2 Macros
+ */
+#define MPL3115A2           0x60 // 7-bit I2C address
+#define STATUS              0x00
+#define OUT_T_MSB           0x04
+#define OUT_T_LSB           0x05
+#define OUT_T_DELTA_MSB     0x0A
+#define OUT_T_DELTA_LSB     0x0B
+#define WHO_AM_I            0x0C
+#define CTRL_REG1           0x26
+
+
 //extern Mode state;
 extern uint8_t hours;
 extern uint8_t minutes;
@@ -23,7 +36,8 @@ extern uint8_t buttonCount;
 extern CircBuf_t * RXBuf;
 extern CircBuf_t * TXBuf;
 
-
+extern CircBuf_t * RXBuf_i2c;
+extern CircBuf_t * TXBuf_i2c;
 
 void configure_SystemClock(){
     CS-> KEY = 0x695A; //Unlock module for register access
@@ -51,6 +65,111 @@ void configure_uart(){
     EUSCI_A0->IFG &= ~(BIT1 | BIT0);
     UCA0IE |= (BIT0 | BIT1);  //Turn on interrupts for RX and TX
     NVIC_EnableIRQ(EUSCIA0_IRQn);
+}
+
+void send_byte(uint8_t data) {
+/*
+:param: uint8_t data: byte to be sent over UART
+ */
+    EUSCI_A0->TXBUF = data;
+}
+
+void configure_i2c(){
+    /*
+     * UCB0SDA -> P1.6
+     * UCB0SCL -> P1.7
+     *
+     * MPL3115A2:
+     *  - I2C Address:  60h
+     *  - 8-bit read:   C1h
+     *  - 8-bit write:  C0h
+     */
+
+    P1->SEL0 |= (BIT6 | BIT7);
+
+    // enable reset, I2C, SYNC, Master Mode, and SMCLK
+    EUSCI_B0->CTLW0 |= (EUSCI_B_CTLW0_SWRST | EUSCI_B_CTLW0_MODE_3 |
+            EUSCI_B_CTLW0_SYNC| EUSCI_B_CTLW0_MST) | EUSCI_B_CTLW0_UCSSEL_2;
+
+    // disable START condition, do not generate STOP, single master, using 7-bit addy's
+    EUSCI_B0->CTLW0 &= ~(EUSCI_B_CTLW0_TXSTT | EUSCI_B_CTLW0_TXSTP |
+            EUSCI_B_CTLW0_MM | EUSCI_B_CTLW0_SLA10 | EUSCI_B_CTLW0_A10);
+
+    EUSCI_B0->I2CSA = 0x60; // MPL3115A2's address
+    EUSCI_B0->BRW = 0x0014; // SMCLK /20 = 400k
+    EUSCI_B0->CTLW0 &= ~(EUSCI_B_CTLW0_SWRST);
+
+    // enable RX, TX, and NACK interrupts
+    EUSCI_B0->IE |= (EUSCI_B_IE_RXIE0 | EUSCI_B_IE_TXIE0 | EUSCI_B_IE_NACKIE);
+    EUSCI_B0->IFG = 0; // clear flags
+    NVIC_EnableIRQ(EUSCIB0_IRQn);
+}
+
+void write_i2c(uint8_t reg, uint8_t val) {
+    addItemCircBuf(TXBuf_i2c, reg);
+    addItemCircBuf(TXBuf_i2c, val);
+//    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_SWRST;     // enable
+    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TR;        // set to write/transmitter
+    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTT;     // send start and addy
+    EUSCI_B0->IFG |= EUSCI_B_IFG_TXIFG0;        // send
+    // block?
+    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTP;     // send stop
+//    EUSCI_B0->CTLW0 &= ~(EUSCI_B_CTLW0_SWRST);  // disable
+}
+
+void read_i2c(uint8_t msg, uint8_t bytes) {
+//    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_SWRST;     // enable
+    EUSCI_B0->CTLW0 &= ~(EUSCI_B_CTLW0_TR);     // set to receiver
+    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTT;     // send start and addy
+    addItemCircBuf(TXBuf_i2c, msg);             // load msg
+    EUSCI_B0->IFG |= EUSCI_B_IFG_TXIFG0;        // send msg
+    // block?
+    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTT;     // send restart
+    while(EUSCI_B0->STATW & EUSCI_B_STATW_BCNT_MASK != (bytes << 8)); // read specified amount of bytes
+    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTP;          // send stop
+//    EUSCI_B0->CTLW0 &= ~(EUSCI_B_CTLW0_SWRST);  // disable
+}
+
+// triggers a sample on the temperature sensor
+void trigger_sample_mpl() {
+    // set OST low
+    read_i2c(CTRL_REG1, 1);
+    while(isEmpty(RXBuf_i2c)); // wait for value back
+    uint8_t setting = removeItem(RXBuf_i2c);
+    setting &= ~(1 << 1); // clear OST bit
+    write_i2c(CTRL_REG1, setting);
+
+    // set OST high again
+    read_i2c(CTRL_REG1, 1);
+    while(isEmpty(RXBuf_i2c)); // wait for value back
+    setting = removeItem(RXBuf_i2c);
+    setting |= (1 << 1);        // set OST bit
+    write_i2c(CTRL_REG1, setting);
+}
+
+uint8_t read_temp_c() {
+    trigger_sample_mpl();
+    read_i2c(STATUS, 1);
+    while(isEmpty(RXBuf_i2c)); // wait for value back
+    if(removeItem(RXBuf_i2c) & (1 << 1) == 0) {
+        return 0;
+    }
+    read_i2c(OUT_T_MSB, 2);
+    while(RXBuf_i2c->num_items < 2); // wait to get two bytes from MPL
+    uint8_t lsb_temp = removeItem(RXBuf_i2c);
+    uint8_t msb_temp = removeItem(RXBuf_i2c);
+    // convert
+    lsb_temp = (lsb_temp >> 4) / 16;
+    return (msb_temp + lsb_temp);
+}
+
+uint8_t convert_to_fahrenheit(uint8_t temp_c) {
+    return (temp_c * 9) / 5 + 32;
+}
+
+uint8_t read_temp_f(){
+    P1->OUT ^= BIT0;
+    return convert_to_fahrenheit(read_temp_c());
 }
 
 void configure_buttons() {
@@ -85,7 +204,7 @@ void configure_leds() {
     P2->OUT &= ~(BIT0 | BIT1 | BIT2);
 }
 
-/* set all unused pins to low for power reasons */
+/* set all unused pins low */
 void configure_all_pins() {
     P1->DIR &= ~(BIT0 | BIT1 | BIT2 | BIT3 | BIT4 | BIT5 | BIT6 | BIT7);
     P1->OUT &= ~(BIT0 | BIT1 | BIT2 | BIT3 | BIT4 | BIT5 | BIT6 | BIT7);
@@ -166,12 +285,37 @@ void EUSCIA0_IRQHandler(){
         addItemCircBuf(RXBuf, EUSCI_A0->RXBUF);
     }
     if (EUSCI_A0->IFG & BIT1){
-        //Transmit Stuff
         if(isEmpty(TXBuf)) {
             EUSCI_A0->IFG &= ~BIT1;
             return;
         }
-//        sendByte(removeItem(TXBuf));
         EUSCI_A0->TXBUF = removeItem(TXBuf);
+    }
+}
+
+// I2C interrupts
+void EUSCIB0_IRQHandler() {
+    // NACK received
+    if(EUSCI_B0->IFG & EUSCI_B_IFG_NACKIFG) {
+        EUSCI_B0->IFG &= ~(EUSCI_B_IFG_NACKIFG); // ACK not NACK
+        EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTT; // send start message
+    }
+
+    // transmitting
+    if(EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG0) {
+        EUSCI_B0->IFG &= ~(EUSCI_B_IFG_TXIFG0);
+        if(isEmpty(TXBuf_i2c)){                    // if circbuf is empty
+            EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTP; // transmit stop message
+            EUSCI_B0->IFG &= ~(EUSCI_B_IFG_TXIFG0); // clear flag
+        }
+        else { // if not empty, send a byte
+            EUSCI_B0->TXBUF = removeItem(TXBuf_i2c); // load what's in buffer
+        }
+    }
+
+    // receiving
+    if(EUSCI_B0->IFG & EUSCI_B_IFG_RXIFG0) {
+        EUSCI_B0->IFG &= ~(EUSCI_B_IFG_RXIFG0); // clear flag
+        addItemCircBuf(RXBuf_i2c, EUSCI_B0->RXBUF);    // throw in circbuf
     }
 }
